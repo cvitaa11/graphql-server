42747b8bc52d22b265f1dc0c64b0a0cff1ab2ff0
Ivan <ivancvitkovic98@gmail.com>
2019-12-26 16:14:15 +0100

    added dependencies

---
+++ schema.js
@@ -0,0 +1,10 @@
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLSchema,
+  GraphQLList
+} = require("graphql");
+
+module.exports = new GraphQLSchema({});
